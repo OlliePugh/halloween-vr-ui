bcb0c9809fc0c0677b7aa9f7cabebe223405f633
Ollie Pugh <oliver.pugh@mac.com>
2022-10-21 17:52:13 +0100

    Added file upload and download

--- src/MapCreator/index.js
+++ src/MapCreator/index.js
@@ -25,8 +25,43 @@ const MapCreator = ({ width, currentTool, nextModule, tiles, setTiles }) => {
         [currentTool, tiles, setTiles]
     );
 
+    const downloadMap = () => {
+        // create file in browser
+        const json = JSON.stringify(tiles, null, 2);
+        const blob = new Blob([json], { type: "application/json" });
+        const href = URL.createObjectURL(blob);
+        // create "a" HTLM element with href to file
+        const link = document.createElement("a");
+        link.href = href;
+        link.download = "map.json";
+        document.body.appendChild(link);
+        link.click();
+        // clean up "a" element & remove ObjectURL
+        document.body.removeChild(link);
+        URL.revokeObjectURL(href);
+    };
+
+    const uploadMap = (e) => {
+        const fileReader = new FileReader();
+        fileReader.readAsText(e.target.files[0], "UTF-8");
+        fileReader.onload = (e) => {
+            try {
+                setTiles(JSON.parse(e.target.result));
+            } catch (e) {
+                alert("Couldnt load map file!");
+            }
+        };
+    };
+
     return (
         <>
+            <Button variant="contained" onClick={downloadMap}>
+                Save Map
+            </Button>
+            <Button variant="contained" component="label" onChange={uploadMap}>
+                Load Map
+                <input type="file" hidden />
+            </Button>
             <Button
                 variant="contained"
                 onClick={async () => {
